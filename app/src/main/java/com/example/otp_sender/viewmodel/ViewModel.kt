2fa95bb042103bdd5d6f3ab8f6a263e57deecfc4
Jonty16117 <jontykantiwal@gmail.com>
2021-03-03 11:52:01 +0530

    made changes to mainViewModel(added new functions)

--- app/src/main/java/com/example/otp_sender/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/otp_sender/viewmodel/ViewModel.kt
@@ -1,9 +1,34 @@
 package com.example.otp_sender.viewmodel
 
+import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.otp_sender.model.DataModel
+import com.example.otp_sender.model.Repository
 
 class MainViewModel : ViewModel() {
+
+    var liveData: LiveData<DataModel>? = null
+
+    fun insertData(
+        context: Context, 
+        firstName: String, 
+        lastName: String,
+        contactNo: Int,
+        otp: Int) {
+
+       Repository.insertData(
+        context: Context, 
+        firstName: String, 
+        lastName: String,
+        contactNo: Int,
+        otp: Int)
+    }
+
+    fun getHistory(context: Context) : LiveData<DataModel>? {
+        liveData = Repository.getHistory(context)
+        return liveData
+    }
 
 }
